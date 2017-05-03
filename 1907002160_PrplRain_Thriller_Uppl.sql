720820948e10a1105251a4e0901857ec7268f26c
danielarnarsson <danielar216@nemi.tskoli.is>
2017-05-03 13:47:55 +0000

    Lagaði villur sem voru í txt file-inu

---
+++ 1907002160_PrplRain_Thriller_Uppl.sql
@@ -0,0 +1,82 @@
+INSERT INTO lag(nafn, flytjandi, hofundur, tegund, diskur, lengd, texti, utgafudagur)
+VALUES
+('Let''s go crazy','The Revolution','Prince','R&B/Soul','Purple Rain','4.39','...And if de-elevator tries to bring you down
+Go crazy (Punch a higher floor!)...','1984-07-25'),
+('Take me with U','The Revolution','Prince','Pop','Purple Rain','3.54','...I don''t care where we go 
+I don''t care what we do 
+I don''t care pretty baby 
+Just take me with you...','1984-07-25'),
+('The Beautiful Ones','The Revolution','Prince','Pop','Purple Rain','5.14','...You make me so confused
+The beautiful ones
+You always seem to lose...','1984-07-25'),
+('Computer Blue','The Revolution','Prince, John L. Nelson, Weny Melvoin, Lisa Coleman','Pop','Purple Rain','3.59','...Where is my love life?
+Tell me, tell me
+Where has it gone?
+Somebody please, please tell me what the hell is wrong...','1984-07-25'),
+('Darling Nikki','The Revolution','Prince','Pop','Purple Rain','4.14','...Come back Nikki come back
+Your darling little prince
+Want to grind, grind, grind....','1984-07-25'),
+('When Doves Cry','The Revolution','Prince','Pop','Purple Rain','5.53','...She''s never satisfied (She''s never satisfied)
+Why do we scream at each other
+This is what it sounds like
+When doves cry...','1984-05-16'),
+('I Would Die 4 U','The Revolution','Prince','Pop','Purple Rain','2.49','...Cause you, I would die for you, yeah
+Darling if you want me to
+You, I would die for you...','1984-07-25'),
+('Baby I''m a Star','The Revolution','Prince','Pop','Purple Rain','4.25','...Baby, I''m a star
+Might not know it now
+Baby, but I are, I''m a star
+I don''t want to stop, till I reach the top
+Sing it, we all are a star...','1984-07-25'),
+('Purple Rain','The Revolution','Prince','Rock/R&B/Gospel/orchestral music','Purple Rain','8.41','...I only wanted to see you
+Laughing in the purple rain
+Purple rain, purple rain
+Purple rain, purple rain
+Purple rain, purple rain...','1984-07-25'),
+
+('Wanna Be Startin'' Somethin''','Michael Jackson','Michael Jackson','Post-disco/funk','Thriller','6.03','I said you wanna be startin'' somethin''
+You got to be startin'' somethin''
+I said you wanna be startin'' somethin''
+You got to be startin'' somethin''...','1983-05-08'),
+('Baby Be Mine','','Rod Temperton','Disco','Thriller','4.20','...So baby, be mine (baby you gotta be mine)
+And girl I''ll give you all I got to give
+So baby, be my girl (all the time)
+And we can share this ecstasy
+As long as we believe in love...','1982-11-30'),
+('The Girl Is Mine','Michael Jackson, Paul McCartney','Michael Jackson','Soft rock','Thriller','3.42','...Because she''s mine
+The doggone girl is mine
+Don''t waste your time
+Because the doggone girl is mine...','1982-10-18'),
+('Thriller','Michael Jackson','Rod Temperton','Disco/Funk','Thriller','5.58','...''Cause this is thriller, thriller night
+There ain''t no second chance against the thing with
+Forty eyes
+You know it''s thriller, thriller night
+You''re fighting for your life inside a killer
+Thriller tonight...','1984-01-23'),
+('Beat it','Michael Jackson','Michael Jackson','Dance-rock','Thriller','4.18','...Just beat it, beat it, beat it, beat it
+No one wants to be defeated
+Showin'' how funky and strong is your fight
+It doesn''t matter who''s wrong or right
+Just beat it, beat it
+Just beat it, beat it
+Just beat it, beat it
+Just beat it, beat it...','1983-02-14'),
+('Billie Jean','Michael Jackson','Michael Jackson','Post-disco','Thriller','4.54','...Billie Jean is not my lover
+She''s just a girl who claims that I am the one
+But the kid is not my son
+She says I am the one, but the kid is not my son...','1983-01-02'),
+('Human Nature','Michael Jackson','Steve Porcaro, John Bettis','Soft rock','Thriller','4.06','If they say,
+Why, why, tell ''em that it''s human nature
+Why, why, does he do it that way
+If they say,
+Why, why, tell ''em that it''s human nature
+Why, why does he do me that way','1983-07-03'),
+('P.Y.T. (Pretty Young Thing)','Michael Jackson','James Ingram, Jones','Disco/funk','Thriller','3.59','...I want to love you (P-Y-T)
+Pretty young thing
+You need some lovin'' (T-L-C)
+Tender lovin'' care
+And I''ll take you there...','1983-08-19'),
+('The Lady in My Life','Michael Jackson','Rod Temperton','Pop','Thriller','4.59','There''ll be no darkness tonight
+Lady our love will shine
+Lighting the night
+Just put your trust in my heart...','1982-??-??');
